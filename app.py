ce0c457352c7d1c2ccd43466a4806da485b360a7
Connor Carew <connorcarew@usf.edu>
2025-11-23 20:46:01 -0500

    Add user input prompt

--- app.py
+++ app.py
@@ -2,3 +2,5 @@ print("Welcome to my Python program!")
 name = input("What is your name? ")
 print(f"Hello, {name}! Nice to meet you.")
 # Add project folder and initial app.py with welcome message
+cats_patted = input("How many cats have you patted today? ")
+print(f"You patted {cats_patted} cat{'s' if cats_patted != '1' else ''} today.")
